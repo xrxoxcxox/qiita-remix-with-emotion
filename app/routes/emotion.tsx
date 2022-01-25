df3ad082d5f69bc36ee69ee1bf124c84b0eca92e
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-26 00:46:53 +0900

    Add Button component

--- app/routes/emotion.tsx
+++ app/routes/emotion.tsx
@@ -1,5 +1,10 @@
+import { Button } from "~/components/Button";
+
 export default function Emotion() {
   return (
-    <h1>Emotion page</h1>
-  )
+    <>
+      <h1>Emotion page</h1>
+      <Button>test</Button>
+    </>
+  );
 }
