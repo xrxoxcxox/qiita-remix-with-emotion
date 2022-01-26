fe78dea8a09a0eace39bdb9559a2c9d8112f1211
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-26 23:29:46 +0900

    Add Navigation component

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,21 @@ export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
+export function links() {
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:wght@400;700&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/icon?family=Material+Icons+Round",
+    },
+  ];
+}
+
 export default function App() {
   return (
     <html lang="en">
--- app/routes/emotion.tsx
+++ app/routes/emotion.tsx
@@ -2,6 +2,7 @@
 import { Fragment } from "react";
 import { jsx, css } from "@emotion/react";
 import { Header } from "~/components/Header";
+import { Navigation } from "~/components/Navigation";
 import { Footer } from "~/components/Footer";
 
 export default function Emotion() {
@@ -9,6 +10,7 @@ export default function Emotion() {
     <Fragment>
       <Header />
       <div css={styles.contents}>
+        <Navigation css={styles.navigation} />
         <aside css={styles.sidebar}>
           <Footer css={styles.footer} />
         </aside>
@@ -25,6 +27,14 @@ const styles = {
     grid-template-columns: repeat(12, minmax(48px, 80px));
     justify-content: center;
     padding: 24px 16px 48px;
+  `,
+  navigation: css`
+    align-self: start;
+    display: flex;
+    flex-direction: column;
+    grid-area: navigation;
+    position: sticky;
+    top: 88px;
   `,
   sidebar: css`
     grid-area: sidebar;
